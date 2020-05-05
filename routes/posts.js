7ace0af62d3209b178fff02b303ae2ee871f82ab
myth <mythsahalan@yahoo.com>
2020-05-05 17:37:07 +0600

    POST and GET added

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const router = express.Router()
-const Post = require('../models/post')
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post");
 
+//get all posts
+router.get("/", (req, res) => {
+  const posts = Post.find()
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
 
+//create and save post
+router.post("/", (req, res) => {
+  Post.create(req.body)
+    .then((post) => {
+      res.send(post);
+    })
+    .catch((err) => res.status(404).send("enter both title and description"));
+});
 
-
-
-
-
-module.exports = router
-
+module.exports = router;
